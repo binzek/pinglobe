0a5ca1dca6bfaf1126c723dfa38ea2710dab267a
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-06 17:32:42 +0530

    feat: implement firebase integration

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFiPzE0igPmeSA_7bKGHTXQrVTkIQhFD0",
+  authDomain: "pinglobe-chat.firebaseapp.com",
+  projectId: "pinglobe-chat",
+  storageBucket: "pinglobe-chat.appspot.com",
+  messagingSenderId: "526144985822",
+  appId: "1:526144985822:web:2687ba2195296ceab0c04f",
+  measurementId: "G-2HXPCBK7MT",
+};
+
+const app = initializeApp(firebaseConfig);
