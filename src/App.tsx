34182ac799b5567c4b4e0e68ec01cd59458c151b
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-06 19:06:15 +0530

    feat: add conditional rendering of homepage and chatpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 // Library imports
-import { FC } from "react";
+import { FC, useState } from "react";
 
 // Local imports
-import { HomePage } from "./components";
+import { HomePage, ChatPage } from "./components";
+import { auth } from "./config/firebase";
 
 const App: FC = () => {
-  return (
-    <div>
-      <HomePage />
-    </div>
+  const [currentPage, setCurrentPage] = useState(<div />);
+
+  auth.onAuthStateChanged((user) =>
+    setCurrentPage(user ? <ChatPage /> : <HomePage />)
   );
+
+  return <div>{currentPage}</div>;
 };
 
 export default App;
