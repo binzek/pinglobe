8d6f978d4991d63811d92ce9f3c67ecd2ea8f1b0
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-09 19:31:31 +0530

    feat: create footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
 import { FC } from "react";
 
 const Footer: FC = () => {
-  return <div>Footer</div>;
+  let currentYear = new Date().getFullYear();
+
+  return (
+    <div className="flex flex-col items-center gap-2 bg-light-palette-gray pt-3 font-inter text-sm text-light-palette-black dark:bg-dark-palette-gray dark:text-dark-palette-white">
+      <p className="font-light">{currentYear} &copy; Abdul Wajid</p>
+      <div className="flex gap-4">
+        <a href="https://github.com/wajid-nv/pinglobe" target="_blank">
+          <span className="material-symbols-rounded font-extralight">code</span>
+        </a>
+        <a href="https://wajid.me/" target="_blank">
+          <span className="material-symbols-rounded font-extralight">
+            alternate_email
+          </span>
+        </a>
+        <a href="https://buymeacoffee.com/wajidnv/" target="_blank">
+          <span className="material-symbols-rounded font-extralight">
+            coffee
+          </span>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
