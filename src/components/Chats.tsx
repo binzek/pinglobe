b91c03ef1766ca570d7d36519ae7eecf9e4e412d
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-10 16:50:04 +0530

    fix: add key property for chats array elements

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -26,9 +26,9 @@ const Chats: FC = () => {
   return (
     <div>
       <div className="flex flex-col gap-2">
-        {messageDetails.map((detailObj) =>
+        {messageDetails.map((detailObj, index) =>
           detailObj.userId === auth.currentUser?.uid ? (
-            <div className="flex items-end gap-1">
+            <div className="flex items-end gap-1" key={index}>
               <p className="hide-scrollbar ml-auto w-2/3 overflow-scroll whitespace-pre-line rounded-lg bg-light-palette-gray p-2 font-inter text-light-palette-black dark:bg-dark-palette-gray dark:text-dark-palette-white">
                 {detailObj.message}
               </p>
@@ -39,7 +39,7 @@ const Chats: FC = () => {
               />
             </div>
           ) : (
-            <div className="flex items-end gap-1">
+            <div className="flex items-end gap-1" key={index}>
               <img
                 src={detailObj.imageUrl || UserIcon}
                 alt="User profile picture"
