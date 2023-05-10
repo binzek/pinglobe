3d2ddcd90e225c9c706915af0f4c63f647c56532
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-10 17:25:10 +0530

    feat: redesign footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,23 +4,27 @@ const Footer: FC = () => {
   let currentYear = new Date().getFullYear();
 
   return (
-    <div className="flex flex-col items-center gap-2 bg-light-palette-gray pb-1 pt-3 font-inter text-sm text-light-palette-black dark:bg-dark-palette-gray dark:text-dark-palette-white">
-      <div className="flex gap-4">
+    <div className="mb-2 flex items-end justify-between">
+      <div className="ml-3 flex flex-col gap-3">
         <a href="https://github.com/wajid-nv/pinglobe" target="_blank">
-          <span className="material-symbols-rounded font-extralight">code</span>
+          <span className="material-symbols-rounded font-extralight text-light-palette-black duration-100 hover:text-red-600 dark:text-dark-palette-white dark:hover:hover:text-red-600">
+            code
+          </span>
         </a>
         <a href="https://wajid.me/" target="_blank">
-          <span className="material-symbols-rounded font-extralight">
+          <span className="material-symbols-rounded font-extralight text-light-palette-black duration-100 hover:text-blue-500 dark:text-dark-palette-white dark:hover:hover:text-blue-500">
             alternate_email
           </span>
         </a>
         <a href="https://buymeacoffee.com/wajidnv/" target="_blank">
-          <span className="material-symbols-rounded font-extralight">
+          <span className="material-symbols-rounded font-extralight text-light-palette-black duration-100 hover:text-yellow-400 dark:text-dark-palette-white dark:hover:hover:text-yellow-400">
             coffee
           </span>
         </a>
       </div>
-      <p className="font-light">{currentYear} &copy; Abdul Wajid</p>
+      <p className="vertical-text mr-1 -rotate-180 text-sm font-light text-light-palette-black dark:text-dark-palette-white">
+        {currentYear} &copy; Abdul Wajid
+      </p>
     </div>
   );
 };
