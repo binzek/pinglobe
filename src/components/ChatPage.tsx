51d464e95bc7055138382bf434fd0d639901f8b1
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-10 17:47:36 +0530

    fix: fix chatpage layout

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -6,7 +6,7 @@ import { Chats } from "./";
 
 const ChatPage: FC = () => {
   return (
-    <div className="mx-auto mt-3 w-[96%] md:w-2/3 lg:w-1/2 xl:w-1/3">
+    <div className="hide-scrollbar mx-auto mt-3 w-[96%] overflow-y-scroll md:w-2/3 lg:w-1/2 xl:w-1/3">
       <Chats />
     </div>
   );
