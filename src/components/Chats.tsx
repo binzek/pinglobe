20d4f3070fc90075c3201b94c1e77fd09e290377
Abdul Wajid <abdulwajidnv@gmail.com>
2023-05-10 14:31:13 +0530

    feat: give style for chats

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -3,8 +3,9 @@ import { FC, useEffect, useState } from "react";
 import { getDocs, collection, orderBy, query } from "firebase/firestore";
 
 // Local imports
-import { db } from "../config/firebase";
+import { auth, db } from "../config/firebase";
 import { NewMessage } from ".";
+import UserIcon from "../assets/user.png";
 
 const Chats: FC = () => {
   const [messageDetails, setMessageDetails] = useState<any[]>([]);
@@ -24,10 +25,32 @@ const Chats: FC = () => {
 
   return (
     <div>
-      <div>
-        {messageDetails.map((detailObj) => (
-          <p>{detailObj.message}</p>
-        ))}
+      <div className="mx-auto flex w-[96%] flex-col gap-2 lg:w-[97%] xl:w-[98%] 2xl:w-[99%]">
+        {messageDetails.map((detailObj) =>
+          detailObj.userId === auth.currentUser?.uid ? (
+            <div className="flex items-end gap-1">
+              <p className="hide-scrollbar ml-auto w-2/3 overflow-scroll whitespace-pre-line rounded-lg bg-light-palette-gray p-2 font-inter text-light-palette-black dark:bg-dark-palette-gray dark:text-dark-palette-white">
+                {detailObj.message}
+              </p>
+              <img
+                src={auth?.currentUser?.photoURL || UserIcon}
+                alt="User profile picture"
+                className="h-9 rounded-full"
+              />
+            </div>
+          ) : (
+            <div className="flex items-end gap-1">
+              <img
+                src={auth?.currentUser?.photoURL || UserIcon}
+                alt="User profile picture"
+                className="h-9 rounded-full"
+              />
+              <p className="hide-scrollbar w-2/3 overflow-scroll whitespace-pre-line rounded-lg bg-light-palette-white p-2 font-inter text-light-palette-black dark:bg-dark-palette-black dark:text-dark-palette-white">
+                {detailObj.message}
+              </p>
+            </div>
+          )
+        )}
       </div>
       <NewMessage getMessages={getMessages} />
     </div>
