f6af987a64b42cfb7333bd28ecf1aa17f3d86e33
Abdul Wajid <71168387+binzek@users.noreply.github.com>
2025-03-08 14:06:17 +0530

    update footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,17 @@ import { FC } from "react";
 const Footer: FC = () => {
   return (
     <div className="fixed bottom-0 left-1/2 flex -translate-x-1/2 justify-center gap-3">
-      <a href="https://github.com/wajid-nv/pinglobe/">
+      <a href="https://github.com/binzek/pinglobe/">
         <span className="material-symbols-rounded font-extralight text-gray-400 hover:text-red-500">
           code
         </span>
       </a>
-      <a href="https://wajid.me/">
+      <a href="https://www.binzek.com/">
         <span className="material-symbols-rounded font-extralight text-gray-400 hover:text-blue-500">
           alternate_email
         </span>
       </a>
-      <a href="https://buymeacoffee.com/wajidnv/">
+      <a href="https://buymeacoffee.com/binzek/">
         <span className="material-symbols-rounded font-extralight text-gray-400 hover:text-yellow-500">
           coffee
         </span>
